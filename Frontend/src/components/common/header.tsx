26e29152d0536903b62f231e83864dd38c983e72
lnart <lennart.pafel@code.berlin>
2024-12-17 16:27:45 +0100

    FEAT: add header component

---
+++ Frontend/src/components/common/header.tsx
@@ -0,0 +1,44 @@
+
+import { 
+    AppBar, 
+    Toolbar, 
+    Typography, 
+  } from '@mui/material'
+  import StorefrontIcon from '@mui/icons-material/Storefront'
+  
+  export default function Header() {
+    return (
+      <AppBar position="static"
+      sx={{
+        width: "100%",
+        left: 0,
+        right: 0
+      }}
+      >
+          <Toolbar sx={{ width: '100%' }}>
+            <StorefrontIcon 
+              sx={{ 
+                display: 'flex', 
+                mr: 1,
+                fontSize: '2rem'
+              }} 
+            />
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{
+                fontWeight: 700,
+                letterSpacing: '.1rem',
+                color: 'inherit',
+                textDecoration: 'none'
+              }}
+              
+            >
+              Yard Sale Flipper
+            </Typography>
+          </Toolbar>
+      </AppBar>
+    )
+  }
+  
