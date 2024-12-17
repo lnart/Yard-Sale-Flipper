30b77e8252dcf56e7156559374fbf8d818c1fc8c
lnart <lennart.pafel@code.berlin>
2024-12-17 16:26:18 +0100

    FEAT: configure app.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,34 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { 
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  Box
+} from '@mui/material'
+import Header from './components/common/header'
+import {Home} from './pages/home/home'
+import {LandingPage} from './pages/landingPage/landingPage'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2196f3',
+      dark: '#1976d2',
+      light: '#64b5f6',
+    },
+    text: {
+      primary: '#2c3e50',
+      secondary: '#546e7a',
+    },
+    background: {
+      default: '#f8f9fa',
+      paper: '#ffffff',
+    },
+    secondary: {
+      main: '#5cbed1'
+    }
+  },
+  typography: {
+    fontFamily: '"Inter", "Roboto", "Helvetica", "Arial", sans-serif',
+    h2: {
+      fontSize: '3.5rem',
+      [createTheme().breakpoints.down('md')]: {
+        fontSize: '2.5rem',
+      },
+    },
+    h3: {
+      fontSize: '2.5rem',
+      [createTheme().breakpoints.down('md')]: {
+        fontSize: '2rem',
+      },
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '8px',
+          textTransform: 'none',
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: '16px',
+          boxShadow: '0 4px 12px rgba(0,0,0,0.05)',
+        },
+      },
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+    
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3860233258250214"
+       crossOrigin="anonymous"></script>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Box sx={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          minHeight: '100vh',
+          margin: 0,
+          padding: 0,
+          width: '100%'
+        }}>
+          <Header />
+          <Box component="main" sx={{ p: 3 }}>
+            <Routes>
+              <Route path="/search" element={<Home />} />
+              <Route path="/" element={<LandingPage />}></Route>
+            </Routes>
+          </Box>
+        </Box>
+      </BrowserRouter>
+    </ThemeProvider>
+    </div>
   )
 }
 
