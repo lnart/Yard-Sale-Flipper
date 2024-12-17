2309bc478f638e3df8aa007ede9cb4ce70998d5d
lnart <lennart.pafel@code.berlin>
2024-12-18 00:23:38 +0100

    change currency

--- Frontend/src/components/features/marketStats.tsx
+++ Frontend/src/components/features/marketStats.tsx
@@ -62,7 +62,7 @@ import {
     const formatCurrency = (amount: number) => {
       return new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: 'USD',
+        currency: 'EUR',
         maximumFractionDigits: 0,
       }).format(amount);
     };
