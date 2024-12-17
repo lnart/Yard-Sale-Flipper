eb2a853f37aaa2dd912d9d65a947803d8031a2c0
lnart <lennart.pafel@code.berlin>
2024-12-17 16:30:05 +0100

    FEAT: add landingpage

---
+++ Frontend/src/pages/landingPage/landingPage.tsx
@@ -0,0 +1,9 @@
+import LandingPage from "../../components/features/landingPage";
+
+export const HomePage = () => {
+    return (
+        <LandingPage></LandingPage>
+    )
+}
+
+export { LandingPage };
